08ece2c16946367c56e3339b939e74ddac89b09a
eeno <iancorcoran87@gmail.com>
2020-04-04 00:04:54 +0100

    Updated script

--- CA2_Ian_Corcoran.R
+++ CA2_Ian_Corcoran.R
@@ -54,7 +54,150 @@ NI_postcode <- subset(NI_postcode, select=c(15,1:15))
 
 
 #f
+#display onlyinfromation where locality, townland and town conatin Limavady
+#using indexing rather than "$" to refernce columns as "grepl" will try and run the comparison operator
+#on each of the entries in the columns
+Limavady_data <- NI_postcode[grepl("LIMAVADY", NI_postcode[["Locality"]]) & 
+                               grepl("LIMAVADY", NI_postcode[["Townland"]]) &
+                               grepl("LIMAVADY", NI_postcode[["Town"]]),]
 
-Limavady_data <- 
+#count rows
+nrow(Limavady_data)
+
+#save in a csv
+write.csv(Limavady_data,"Limavady.csv")
 
 
+#g
+#save the modified postcode data
+write.csv(NI_postcode,"CleanNIPostcodeData.csv")
+
+
+#Section 2 ----------------------------------------------------------------------------------------------------
+
+#Aamalgamate all the crime data from each csv
+
+
+# directory where files are located
+dir <- "./NI Crime Data"
+#list all subfolders
+folders <- list.dirs(dir,full.names = TRUE)
+
+#list all files in the subfolders
+new_data <- sapply(folders[-1], function(x) {
+  df1 <- list.files(x,full.names = TRUE)
+}
+)
+
+ff <- list.files(dir, pattern = "*.csv", recursive = TRUE, full.names = TRUE)
+
+#read all csv files into a datframe
+AllNICrimeData <- do.call(rbind, lapply(ff, read.csv))
+
+
+#b
+str(AllNICrimeData)
+
+#need to drop certain columns
+cols_to_drop <- c("Crime.ID", "Reported.By", "Falls.within", "LSOA.code", "LSOA.name",
+                  "Last.outcome.category", "Context")
+
+
+AllNICrimeData <- AllNICrimeData[,!names(AllNICrimeData) %in% cols_to_drop]
+str(AllNICrimeData)
+
+
+#C shorten crime type names
+
+AllNICrimeData$Crime.type <- as.character(AllNICrimeData$Crime.type)
+
+crime_list <- c("Anti-social behaviour","Bicycle theft", "Burglary","Criminal damage and arson",
+                "DRUGS","Other Theft", "Public order","Robbery", "Shoplifting",
+                "Theft from the person","Vehicle crime", "Violence and sexual offences")
+
+attach(AllNICrimeData)
+AllNICrimeData$Crime.type[Crime.type == "Anti-social behaviour"] <- "ASBO"
+AllNICrimeData$Crime.type[Crime.type == "Bicycle theft"] <- "BITH"
+AllNICrimeData$Crime.type[Crime.type == "Burglary"] <- "BURG"
+AllNICrimeData$Crime.type[Crime.type == "Criminal damage and arson"] <- "CDAR"
+AllNICrimeData$Crime.type[Crime.type == "DRUGS"] <- "DRUG"
+AllNICrimeData$Crime.type[Crime.type == "Other Theft"] <- "OTTH"
+AllNICrimeData$Crime.type[Crime.type == "Public order"] <- "PUBO"
+AllNICrimeData$Crime.type[Crime.type == "Robbery"] <- "ROBY"
+AllNICrimeData$Crime.type[Crime.type == "Shoplifting"] <- "SHOP"
+AllNICrimeData$Crime.type[Crime.type == "Theft from the person"] <- "THPR"
+AllNICrimeData$Crime.type[Crime.type == "Vehicle crime"] <- "VECR"
+AllNICrimeData$Crime.type[Crime.type == "Violence and sexual offences"] <- "VISO"
+AllNICrimeData$Crime.type[Crime.type != crime_list] <- "OTCR"
+detach(AllNICrimeData)
+
+
+#d
+
+
+CrimeFreq <- table(AllNICrimeData$Crime.type)
+
+CrimeFreq <- prop.table(CrimeFreq)
+CrimeFreq
+
+#E
+barplot(height = CrimeFreq,
+        main = "Frequency by Crime Type", 
+        ylab = "Frequency", 
+        xlab = "Crime Type",
+        col = "blue")
+
+
+#F
+AllNICrimeData$Crime.type <- factor(AllNICrimeData$Crime.type, ordered = FALSE)
+
+str(AllNICrimeData)
+
+
+#G
+#drop the words on or near from location
+unique(AllNICrimeData$Location)
+
+drop_words <- c("On or near")
+
+AllNICrimeData$Location <-  gsub(drop_words, "" ,AllNICrimeData$Location)
+#AllNICrimeData$Location <- sapply(1:nrow(AllNICrimeData), function (x) gsub(drop_words, "" ,AllNICrimeData$Location))
+
+#replace blanks wit na
+AllNICrimeData$Location[AllNICrimeData$Location == " "] <- NA
+sum(is.na(AllNICrimeData$Location))
+str(AllNICrimeData$Location)
+
+
+
+#h create a funtion to combine ni crome data and poulaation and status to the crime data
+
+village <- read.csv("VillageList.csv")
+#rename first column in village list
+names(village)[1] <- "Location"
+
+#testdf <- merge(AllNICrimeData,village, by= "Location")
+get_town_info <- function(df1,df2){
+  
+  #df1$Location <- newcol
+  
+  status_Loc <- df2$STATUS[match(df1$Location, df2$Location)]  
+  pop_loc    <- df2$POPULATION[match(df1$Location, df2$Location)]
+    eval.parent(substitute(df1["Status"] <- status_Loc))
+    eval.parent(substitute(df1["Population"] <- pop_loc))
+  
+  return(df1)
+  
+}
+
+
+get_town_info(AllNICrimeData,village)
+
+unique(status_Loc)
+
+AllNICrimeData$Location <-   trimws(AllNICrimeData$Location)
+
+status_Loc <- (match(AllNICrimeData$Location, village$Location))
+status_Loc <- ifelse(!is.na(status_Loc),status_Loc,NA)
+status_Loc <- na.omit(status_Loc)
+status_Loc
