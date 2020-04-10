2727318b5bd4bfde9f658ab78bebddb77981835a
eeno <iancorcoran87@gmail.com>
2020-04-10 01:15:42 +0100

    update

--- CA2_Ian_Corcoran.R
+++ CA2_Ian_Corcoran.R
@@ -1,7 +1,10 @@
 #Section 1
 #A
 
-NI_postcode <- read.csv("NIPostcodes.csv")
+rm(NI_postcode)
+
+#import data
+NI_postcode <- read.csv("NIPostcodes.csv", header = FALSE)
 
 #total number of rows
 nrow(NI_postcode)
@@ -13,6 +16,8 @@ head(NI_postcode, n=10)
 
 #B
 #add titles to the attributes
+
+#create a vector of column names
 col_names <- c("Organisation Name",
                "Sub-building Name",
                "Building Name",
@@ -28,7 +33,8 @@ col_names <- c("Organisation Name",
                "x-coordinates",
                "y-coordinates",
                "Primary Key ,")
-
+ 
+#use names function to apply all names form col_names vector to dataset
 names(NI_postcode)[1:15] <- col_names
 str(NI_postcode)
 
@@ -40,17 +46,26 @@ str(NI_postcode)
 
 
 #D
-#find the na vlaues in each row
+#find the empty vlaues in each column
+#Use sapply to iterate over all columns and get the sum of the blank values
+blank_count <- sapply(NI_postcode, function(y) sum(length(y[y == ""])))
+#convert to dataframe for easy interpratation
+blank_count <- data.frame(blank_count)
+#view the blank counts
+blank_count
+
+#find the na vlaues in each column
 #Use sapply to iterate over all columns and get the sum of the NA values
 na_count <-sapply(NI_postcode, function(y) sum(length(which(is.na(y)))))
-#convert to dataframe to see the values easier
+#convert to dataframe for easy interpratation
 na_count <- data.frame(na_count)
 na_count
 
 
 #E
 #Use subset to move primary key to begining
-NI_postcode <- subset(NI_postcode, select=c(15,1:15))
+NI_postcode <- subset(NI_postcode, select=c(15,1:14))
+str(NI_postcode)
 
 
 #f
@@ -61,78 +76,80 @@ Limavady_data <- NI_postcode[grepl("LIMAVADY", NI_postcode[["Locality"]]) &
                                grepl("LIMAVADY", NI_postcode[["Townland"]]) &
                                grepl("LIMAVADY", NI_postcode[["Town"]]),]
 
+# reset index
+rownames(Limavady_data) <- 1:nrow(Limavady_data)
 #count rows
 nrow(Limavady_data)
+
+str(Limavady_data)
 
 #save in a csv
 write.csv(Limavady_data,"Limavady.csv")
 
 
 #g
-#save the modified postcode data
+#save the modified postcode data to working directory 
 write.csv(NI_postcode,"CleanNIPostcodeData.csv")
 
 
 #Section 2 ----------------------------------------------------------------------------------------------------
 
-#Aamalgamate all the crime data from each csv
+#A amalgamate all the crime data from each csv file
 
+#let user choose the directory
+directory <- choose.dir(getwd(), caption = "Select folder")
+ 
 
-# directory where files are located
-dir <- "./NI Crime Data"
-#list all subfolders
-folders <- list.dirs(dir,full.names = TRUE)
+#list all csv files in the chosen direcotry
+all_data <- list.files(directory, pattern = "*.csv", recursive = TRUE, full.names = TRUE)
 
-#list all files in the subfolders
-new_data <- sapply(folders[-1], function(x) {
-  df1 <- list.files(x,full.names = TRUE)
-}
-)
+#read all csv files into a datframe using do.call and rbind
+AllNICrimeData <- do.call(rbind, lapply(all_data, read.csv))
+ 
+#cont rows
+nrow(AllNICrimeData)
 
-ff <- list.files(dir, pattern = "*.csv", recursive = TRUE, full.names = TRUE)
-
-#read all csv files into a datframe
-AllNICrimeData <- do.call(rbind, lapply(ff, read.csv))
- nrow(AllNICrimeData)
-
-
-
-#b
 str(AllNICrimeData)
 
-#need to drop certain columns
+#b
+#need to drop certain columns, create a vector of column anmes
 cols_to_drop <- c("Crime.ID", "Reported.By", "Falls.within", "LSOA.code", "LSOA.name",
                   "Last.outcome.category", "Context")
 
-
+#drop any column in the "cols_to_drop" vector
 AllNICrimeData <- AllNICrimeData[,!names(AllNICrimeData) %in% cols_to_drop]
 str(AllNICrimeData)
 
 
-#C shorten crime type names
-
+#C 
+#shorten crime type names
+#convert crime type column to character to allow name change
 AllNICrimeData$Crime.type <- as.character(AllNICrimeData$Crime.type)
 
+#create a vector of crime names mentioned in CA doc
 crime_list <- c("Anti-social behaviour","Bicycle theft", "Burglary","Criminal damage and arson",
                 "Drugs","Other theft", "Public order","Robbery", "Shoplifting",
                 "Theft from the person","Vehicle crime", "Violence and sexual offences")
-
+#atache data frame soto cut out repetitive type
 attach(AllNICrimeData)
+# if the name is not in the crime_list vector then change the name to "OTCR"
 AllNICrimeData$Crime.type[Crime.type != crime_list] <- "OTCR"
+#Change the crime type to the abreviations
 AllNICrimeData$Crime.type[Crime.type == "Anti-social behaviour"] <- "ASBO"
 AllNICrimeData$Crime.type[Crime.type == "Bicycle theft"] <- "BITH"
 AllNICrimeData$Crime.type[Crime.type == "Burglary"] <- "BURG"
 AllNICrimeData$Crime.type[Crime.type == "Criminal damage and arson"] <- "CDAR"
 AllNICrimeData$Crime.type[Crime.type == "Drugs"] <- "DRUG"
-AllNICrimeData$Crime.type[Crime.type == "Other Theft"] <- "OTTH"
+AllNICrimeData$Crime.type[Crime.type == "Other theft"] <- "OTTH"
 AllNICrimeData$Crime.type[Crime.type == "Public order"] <- "PUBO"
 AllNICrimeData$Crime.type[Crime.type == "Robbery"] <- "ROBY"
 AllNICrimeData$Crime.type[Crime.type == "Shoplifting"] <- "SHOP"
 AllNICrimeData$Crime.type[Crime.type == "Theft from the person"] <- "THPR"
 AllNICrimeData$Crime.type[Crime.type == "Vehicle crime"] <- "VECR"
 AllNICrimeData$Crime.type[Crime.type == "Violence and sexual offences"] <- "VISO"
+#detach dataframe
 detach(AllNICrimeData)
-
+ #check unique names to make sure all names were changed
 unique(AllNICrimeData$Crime.type)
 #d
 
@@ -166,98 +183,138 @@ str(AllNICrimeData)
 
 #E
 #drop the words on or near from location
-
-#check the unique locations
-unique(AllNICrimeData$Location)
-
 drop_words <- c("On or near ")
 
+#use gsub fuunction to replace the words in drop vector vector with ""
 AllNICrimeData$Location <-  gsub(drop_words, "" ,AllNICrimeData$Location)
-#AllNICrimeData$Location <- sapply(1:nrow(AllNICrimeData), function (x) gsub(drop_words, "" ,AllNICrimeData$Location))
 
-#replace blanks wit na
+
+#replace blank values with na
 AllNICrimeData$Location[AllNICrimeData$Location == ""] <- NA
+
+
+
 sum(is.na(AllNICrimeData$Location))
-str(AllNICrimeData$Location)
+str(AllNICrimeData)
 
 
 
 
 
 
-#f choose 5000 random varaibles and create a function to return town name
+#f 
+#choose 5000 random varaibles and create a function to return town name that isnt blank
+#create a new dataframe with where there are no NA's in the location columns
 new_df <- AllNICrimeData[!is.na(AllNICrimeData$Location),]
+#set the seed ot 100
 set.seed(100)
+#sample 5000 random rows from the dataset with no NA's in the location 
+#call ne dataframe random_crime_sample
 random_crime_sample <-  new_df[sample(1:nrow(new_df), 5000),]
+
 random_crime_sample
 unique(random_crime_sample$Location)
+unique(random_crime_sample$Crime.type)
 
 
 
+#read in cleanNIPostcode csv file
+CleanNIPostcodeData <- read.csv("CleanNIPostcodeData.csv")
 
-
-#g create a funtion to combine ni crime data and poulaation and status to the crime data
-
+#custom function to find a town
 find_a_town <- function(baseframe, lookupframe){
   
-  #assign(baseframe$Location <- as.character(baseframe$Location))
+  #convert the location in the lookup in baseframe (random_crime_sample) to upper case 
+  #for matching purposes as exact matches will only be returned
+  #used eval.parent as R does not allow for staright dataframe manipulation with a custom function
   eval.parent(substitute( baseframe$Location <- assign(baseframe$Location,toupper(baseframe$Location))))
   
-  
-  
-  
+  #use macth function to lookup the town in the baseframe (random_crime_sample) against
+  #Thotfare in lookupframe (CleanNIPostcodeData)
   town_Loc <- lookupframe$Town[match(baseframe$Location, lookupframe$Primary.Thorfare)]
+  #add Town column
   eval.parent(substitute(baseframe["Town"] <- town_Loc))
   
   return(baseframe)
   
 }
 
-
+#call function with a base frame and a lookupp frame
 find_a_town(random_crime_sample, CleanNIPostcodeData)
 
-str(CleanNIPostcodeData)
-
-CleanNIPostcodeData <- read.csv("CleanNIPostcodeData.csv")
+str(random_crime_sample)
 
 
 
+#g 
+
+#create a function to combine ni crime data and poulation form a village list 
+#to the random crime data
+#read in village data
+village <- read.csv("VillageList.csv")
+#change the irst column to lookup for eas of lookup
+names(village)[1] <- "Location"
+
+#custom function to return town info
 get_town_info <- function(baseframe,lookupframe){
-  
+  #convert the location in the lookup frame (village) to upper case
   lookupframe$Location <- toupper((lookupframe$Location))
   #eval.parent(substitute( baseframe$Location <- assign(baseframe$Location,toupper(baseframe$Location))))
   
+  #use macth function to lookup the town in the baseframe (random_crime_sample) against
+  #location in lookup frame (village) to return the population value
   pop_loc <- lookupframe$POPULATION[match(baseframe$Town, lookupframe$Location)]
-    eval.parent(substitute(baseframe["Population"] <- pop_loc))
+  #add population colum to the baseframe  
+  eval.parent(substitute(baseframe["Population"] <- pop_loc))
   
   return(baseframe)
   
 }
+#call function with baseframe and lookupframe
 get_town_info(random_crime_sample,village)
 
 
+str(random_crime_sample)
+
+
+#h
+#drop the "reported by" column
+random_crime_sample <-  subset(random_crime_sample, select=c(1,3:8))
+#change "town" column to "City-Town-Village"
+names(random_crime_sample)[6] <- "City-Town-Village"
+
+#write the dataframe ot working directory
 write.csv(random_crime_sample,"random_crime_sample.csv")
 
 
 random_crime_sample$Town <- as.character(random_crime_sample$Town)
 
 
+#i
+#split data into belfats data and derry data
 
-
-Belfast_data <- random_crime_sample[random_crime_sample$Town == "BELFAST",]
-Belfast_data <- Belfast_data[complete.cases(Belfast_data$Town),]
+#create a dataframe containing belfast data only
+Belfast_data <- random_crime_sample[random_crime_sample$`City-Town-Village` == "BELFAST",]
+#take only complete cases
+Belfast_data <- Belfast_data[complete.cases(Belfast_data$`City-Town-Village`),]
+#create a table  of crimetype counts using  belfast data
 Belfast_Freq <- table(Belfast_data$Crime.type )
 
-Derry_data <- random_crime_sample[random_crime_sample$Town == "LONDONDERRY",]
-Derry_data <- Derry_data[complete.cases(Derry_data$Town),]
+#creare a dataframe containing belfast data only
+Derry_data <- random_crime_sample[random_crime_sample$`City-Town-Village` == "LONDONDERRY",]
+#take only complete cases
+Derry_data <- Derry_data[complete.cases(Derry_data$`City-Town-Village`),]
+#create a table  of crimetype counts using  derry data data
 Derry_Freq <- table(Derry_data$Crime.type )
 
 str(random_crime_sample)
-
+#using par to display 2 charts side by side
+#displaying in a 1X2 array
 par(mfrow=c(1,2))
 
-plot(  Belfast_Freq ,  #CrimeFreq
-       type = "h",
+#plot the belfast data
+plot(  Belfast_Freq ,  
+       type = "h", #plot as histogram
        main = "Frequency by Crime Type Belfast", 
        ylab = "Frequency", 
        xlab = "Crime Type",
@@ -265,13 +322,13 @@ plot(  Belfast_Freq ,  #CrimeFreq
 
 
 
-
-plot(  Derry_Freq ,  #CrimeFreq
-       type = "h",
+#plot the derry data
+plot(  Derry_Freq ,  
+       type = "h", #plot as histogram
        main = "Frequency by Crime Type Derry", 
        ylab = "Frequency", 
        xlab = "Crime Type",
-       col = "blue")
+       col = "red")
 
-par(mfrow=c(1,2))
+
 
